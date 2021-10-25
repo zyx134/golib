af931f3b28dfb565ee5876a37b9516079831f8d1
yixing <yixing@catddm.com>
2021-10-25 09:51:14 +0000

    添加覆盖文件写入函数

--- _os/os.go
+++ _os/os.go
@@ -14,3 +14,19 @@ func WriteToFile(f *os.File, str string) error {
 	}
 	return write.Flush()
 }
+
+//覆盖文件内容
+//先清空，再写入
+//清空文件然后写入
+func ClearUpAndWrite(filepath string, str string) error {
+	file, err := os.OpenFile(filepath, os.O_WRONLY|os.O_TRUNC, 0600)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	err = WriteToFile(file, str)
+	if err != nil {
+		return err
+	}
+	return nil
+}
